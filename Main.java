7a400c340d1e9c9eddbe1fee82d5aad8550de7f7
Ayham Alhettini <77693971+Ayham-Alhittini@users.noreply.github.com>
2023-11-29 19:08:13 +0300

    Checkout feature developed by (Developer 2)

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +8,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to atypon store");
+        System.out.println();
+        System.out.println("Enter your balance");
+
+        double totalBalance = scanner.nextDouble();
+
+        ArrayList<Product> cartProducts = new ArrayList<>();
 
         while (true) {
             System.out.println("Enter (1) for show all available products");
@@ -15,9 +22,35 @@ public class Main {
             String option = scanner.nextLine();
 
             if (option.equals("1")) {
-                System.out.println("show all available products under creation");///for developer 1
+
+
+                int productIndex = scanner.nextInt() - 1;
+                int wantedQuantity = scanner.nextInt();
+
+                int exampleProductPrice = 12;
+
+                if (totalBalance >= exampleProductPrice * wantedQuantity) {///could buy it
+                    totalBalance -= exampleProductPrice * wantedQuantity;
+
+                    cartProducts.add(new Product("example product", exampleProductPrice, wantedQuantity));
+
+                } else {
+                    System.out.println("Your balance can't afford it");
+                }
+
+
+
+
+
             } else if (option.equals("2")) {
-                System.out.println("checkout cart under creation");///developer 2
+                //Author: Developer 2
+
+                double totalPrice = 0;
+                for (int i = 0; i < cartProducts.size(); i++) {
+                    System.out.println((i + 1) + "- " + cartProducts.get(i).getName() + " X " + cartProducts.get(i).getQuantity());
+                    totalPrice += cartProducts.get(i).getPrice();
+                }
+                System.out.println("Your total price = " + totalPrice);
             } else {
                 System.out.println("Invalid operation please choose (1-2)");
             }
